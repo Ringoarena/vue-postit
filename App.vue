42636821824ca2e0a1ed1243017aeabf2f8e6111
Rikard Virta <rikard.virta90@gmail.com>
2020-05-06 11:28:27 +0200

    implemented list/column-view feature and add note feature

--- App.vue
+++ App.vue
@@ -1,13 +1,67 @@
 <template>
-
+<div class="wrapper">
+    <header>
+        <button
+            v-on:click="toggleView"
+        >L/C</button>
+        <button
+            v-on:click="addNote"
+        >Add Note</button>
+    </header>
+    <main>
+        <section class="postit-container" v-bind:class="{listView: singleColumn}">
+            <postit
+                v-bind:key="index"
+                v-for="(note, index) in notes"
+                v-bind:propsContent="note.content"
+            />
+        </section>
+    </main>
+</div>
 </template>
 
 <script>
-export default {
+import Postit from './Postit'
 
+export default {
+    name: 'App',
+    components: {
+        postit: Postit
+    },
+    data(){
+        return {
+            singleColumn: false,
+            notes: [
+                {content: 'hardcoded content'},
+                {content: 'hardcoded content'},
+                {content: 'hardcoded content'}
+            ]
+        }
+    },
+    methods: {
+        toggleView(){
+            this.singleColumn = !this.singleColumn;
+        },
+        addNote(){
+            this.notes.push({content: 'placeholder content'});
+        }
+    }
 }
 </script>
 
 <style>
+header {
+    border-bottom: 2px solid black;
+}
+.postit-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
+    /* background-color: rgb(216, 216, 216); */
+    margin-top: 1rem;
+}
 
+.listView {
+    grid-template-columns: repeat(1, 1fr);
+}
 </style>
--- Postit.vue
+++ Postit.vue
@@ -1,13 +1,41 @@
 <template>
-  
+<article>
+    <button v-on:click="toggleEditMode">Edit</button>
+    <input type="text" v-bind:class="{hidden: !editMode}" v-model="newContent">
+    <p v-bind:class="{hidden: editMode}">{{propsContent}}</p>
+</article>
 </template>
 
 <script>
 export default {
+    props: {
+        propsContent: String
+    },
+    data(){
+        return {
+            editMode: false,
+            newContent: ''
+        }
+    },
+    methods: {
+        toggleEditMode(){
+            this.editMode = !this.editMode;
+        },
+        fireEditEvent(){
+            this.$emit('editEvent', this.newContent);
+        }
+    }
 
 }
 </script>
 
 <style>
+article{
+    border: 1px solid black;
+}
+
+.hidden{
+    display: none;
+}
 
 </style>
